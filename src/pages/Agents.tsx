a7a9f15a736470bd0777914c3f959c4b1cf6e879
root <mtrhoades0727@gmail.com>
2023-10-17 15:40:47 -1000

    adding content in modal form for each agent on agents page

--- src/pages/Agents.tsx
+++ src/pages/Agents.tsx
@@ -1,96 +1,154 @@
 import { useState } from 'react';
-import ReactCardFlip from 'react-card-flip';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import josephPhoto from '../assets/images/agents/Joseph.jpg';
 import sashaPhoto from '../assets/images/agents/Sasha.jpg';
 import hawanaPhoto from '../assets/images/agents/Hawana.jpg';
 import arielPhoto from '../assets/images/agents/Ariel.jpg';
 import shirleyPhoto from '../assets/images/agents/Shirley.jpg';
+import fremontPhoto from '../assets/images/agents/Fremont.jpg';
 
 const About = () => {
 
-const [isFlipped1, setIsFlipped1] = useState(false);
-const [isFlipped2, setIsFlipped2] = useState(false);
-const [isFlipped3, setIsFlipped3] = useState(false);
-const [isFlipped4, setIsFlipped4] = useState(false);
-const [isFlipped5, setIsFlipped5] = useState(false);
+  const [bodyOpacity, setBodyOpacity] = useState(1);
 
-const flipCard1 = () => {
-  setIsFlipped1(!isFlipped1);
-}
-const flipCard2 = () => {
-  setIsFlipped2(!isFlipped2);
-}
-const flipCard3 = () => {
-  setIsFlipped3(!isFlipped3);
-}
-const flipCard4 = () => {
-  setIsFlipped4(!isFlipped4);
-}
-const flipCard5 = () => {
-  setIsFlipped5(!isFlipped5);
-}
+  const [show, setShow] = useState(false);
+  
+  const handleClose = () => {
+    setShow(false);
+    setBodyOpacity(1);
+  };
 
-
+  const handleShow = () => {
+    setShow(true);
+    setBodyOpacity(0.5);
+  };  
 
   return (
-    <div className="agentsWrapper">
+    <div style={{opacity: bodyOpacity}} className="agentsWrapper">
 
-      <ReactCardFlip flipDirection='horizontal' isFlipped={isFlipped1}>
-        <div className="card card-front" onClick={flipCard1}>
+        <div className="card" onClick={handleShow}>
           <img src={josephPhoto} alt="Joe Blackburn"></img>
           <h3>Joseph Blackburn</h3>
           <h5>Co-Owner</h5>
           <h5>Broker/Agent</h5>
         </div>
-        <div className="card card-back" onClick={flipCard1}>
-          <h3>Back</h3>
-          <p>Joe Blackburn has been part of the Maui Community since 1976. Joe began serving the community as a Maui Police Officer, then later for the Maui Fire Department retiring in 2002 as a Fire Rescue Captain. Joe has been a real estate agent on Maui since 1983, and has a master's degree from the University of Hawaii in Higher Education Administration, bachelor's degree from California State University at Chico in Business Administration and a bachelor's degree from Western Oregon State College in Public Fire Administration. Maui is a beautiful island to raise a family. The land is special for anyone who loves Maui. Having raised three children on Maui brings an exceptional feeling and relationship with the community, which is returned by sharing love for the Island as a real estate agent Joe is a real estate agent who is part of the fabric of the Maui community. Co-chairperson of the Maui Local Emergency Planning Committee, Basketball coach for youth teams, running leagues for our youth.</p>
-        </div>
-      </ReactCardFlip>
+      <Modal backdrop={false} size='xl' centered show={show} onHide={handleClose}>
+        <Modal.Header>
+          <img className="thumbnail" src={josephPhoto} alt="Joe Blackburn"></img>
+          <Modal.Title><h1>Joseph Blackburn</h1></Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <h4 style={{paddingBottom: '2rem'}} >Real Estate Liscense obtained in 1983</h4>
 
-      <ReactCardFlip flipDirection='horizontal' isFlipped={isFlipped2}>
-        <div className="card card-front" onClick={flipCard2}>
+            <h4>Education:</h4>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <h4>Careers:</h4>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <h4>Community:</h4>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+
+            {/* <table className="agentsTable">
+              <tr style={{paddingBottom: '2rem'}}>
+                <th><h4>Education:</h4></th>
+                <td>
+                  <p>University of Hawaii - Masters Degree in Higher Education</p>
+                </td>
+                <td>
+                  <p>California State University - Bachelor Degree in Business Administration</p>
+                </td>
+                <td>
+                  <p>Western Oregon State College - Bachelor Degree in Public Fire Administration</p>
+                </td>
+              </tr>
+              <tr>
+                <th><h4>Careers:</h4></th>
+                <td>
+                  <p>Maui Police Officer</p>
+                </td>
+                <td>
+                  <p>Maui Fire Fighter - Fire Rescue Captain</p>
+                </td>
+                <td>
+                  <p>Maui Electric Company Supervisor</p>
+                </td>
+              </tr>
+              <tr>
+                <th><h4>Community:</h4></th>
+                <td>
+                  <p>Co-chairperson of Maui Local Emergency Planning Committee</p>
+                </td>
+                <td>
+                  <p>Basketball Coach - Recreational Youth Leagues</p>
+                </td>
+              </tr>
+            </table> */}
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      
+        <div className="card">
           <img src={shirleyPhoto} alt="Shirley Blackburn"></img>
           <h3>Shirley Blackburn</h3>
           <h5>Co-Owner</h5>
         </div>
-        <div className="card card-back" onClick={flipCard2}>
+        {/* <div className="card card-back">
           <h3>Back</h3>
-        </div>
-      </ReactCardFlip>
+        </div> */}
 
-      <ReactCardFlip flipDirection='horizontal' isFlipped={isFlipped3}>
-        <div className="card card-front" onClick={flipCard3}>
+        <div className="card">
           <img id="arielImg" src={arielPhoto} alt="Ariel Blackburn"></img>
           <h3>Ariel Blackburn-Rhoades</h3>
           <h5>Agent</h5>
         </div>
-        <div className="card card-back" onClick={flipCard3}>
+        {/* <div className="card card-back">
           <h3>Back</h3>
-        </div>
-      </ReactCardFlip>
+        </div> */}
 
-      <ReactCardFlip flipDirection='horizontal' isFlipped={isFlipped5}>
-        <div className="card card-front" onClick={flipCard5}>
+        <div className="card">
           <img src={sashaPhoto} alt="Sasha Haukoloa"></img>
           <h3>Sasha Haukoloa</h3>
           <h5>Agent</h5>
         </div>
-        <div className="card card-back" onClick={flipCard5}>
+        {/* <div className="card card-back">
           <h3>Back</h3>
-        </div>
-      </ReactCardFlip>
+        </div> */}
 
-      <ReactCardFlip flipDirection='horizontal' isFlipped={isFlipped4}>
-        <div className="card card-front" onClick={flipCard4}>
+        <div className="card">
           <img src={hawanaPhoto} alt="Hawana Blackburn"></img>
           <h3>Hawana Blackburn</h3>
           <h5>Agent</h5>
         </div>
-        <div className="card card-back" onClick={flipCard4}>
+        {/* <div className="card card-back">
           <h3>Back</h3>
+        </div> */}
+
+        <div className="card">
+          <img src={fremontPhoto} alt="Fremont Dog"></img>
+          <h3>Fremont</h3>
+          <h5>Security Guard</h5>
         </div>
-      </ReactCardFlip>
+        {/* <div className="card card-back">
+          <h3>Back</h3>
+        </div> */}
+
 
     </div>
   )
