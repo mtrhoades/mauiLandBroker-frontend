4c2cb08e155b0195974c0d0a8f195465cd55eff4
root <mtrhoades0727@gmail.com>
2024-01-04 11:49:32 -1000

    fixed another... error on html page

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -5,7 +5,11 @@ const SearchResults = () => {
     <div className="searchResultsContainer">
       <h1>Search Results</h1>
 
-      <div className="mci-app-node mci-idx-node mci-idx-node-quickSearch" data-mod="quickSearch" data-property_types="Residential,Condominium,Vacant Land" data-mls_box="true" data-district_list="true" data-price_range="true" data-bed_bath="true"><div style={{textAlign: "center"}}>Loading...</div></div>
+      <div className="mci-app-node mci-idx-node mci-idx-node-quickSearch" data-mod="quickSearch" data-property_types="Residential,Condominium,Vacant Land" data-mls_box="true" data-district_list="true" data-price_range="true" data-bed_bath="true">
+        
+        <div style={{textAlign: "center"}}>Loading...</div>
+        
+      </div>
 
 
       {/* <div className="mci-app-node mci-idx-node mci-idx-node-search" data-mod="search" data-display="featured">
