6dbd9bffe7dee5c9a9d1f57f75fa22393e1a4a90
root <mtrhoades0727@gmail.com>
2024-01-05 09:38:36 -1000

    testing style changes for mci code for listings on amplify

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,45 +1,6 @@
-import {useState, useEffect} from 'react';
-
 const SearchResults = () => {
-
-  // fetch request for mls data
-  // const getMlsdata = async () => {
-  //   try {
-  //     const response = await fetch('https://users.mciserver.com/api/appSetup',
-  //       {
-  //         method: 'GET',
-  //         headers: {
-  //           'Accept': 'application/json',
-  //           'Access-Control-Allow-Origin': '*',
-  //           'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  //         }
-  //       }
-  //     );
-  //     const jsonData = await response.json();
-  //     console.log(jsonData);
-  //   } catch (e: any) {
-  //     console.error(e.message);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   getMlsdata()
-  // }, [])
-
   return (
     <div className="searchResultsContainer">
-      <h1>Search Results</h1>
-
-      {/* <div className="mci-app-node mci-idx-node mci-idx-node-quickSearch" data-mod="quickSearch" data-property_types="Residential,Condominium,Vacant Land" data-mls_box="true" data-district_list="true" data-price_range="true" data-bed_bath="true">
-        
-        <div style={{textAlign: "center"}}>Loading...</div>
-        
-      </div> */}
-
-
-      {/* <div className="mci-app-node mci-idx-node mci-idx-node-search" data-mod="search" data-display="featured">
-        <div style={{textAlign: "center"}}>Loading...</div>
-      </div> */}
 
       <div className="mci-app-node mci-idx-node mci-idx-node-search" data-mod="search" data-display="splash">
         <div style={{textAlign: "center"}}>Loading...</div>
