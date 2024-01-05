1d2f3e826caf84dcafb473f44b30dd986ffa9778
root <mtrhoades0727@gmail.com>
2024-01-05 09:15:10 -1000

    cors issue fixed, checking listings on amplify

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -5,7 +5,7 @@ const SearchResults = () => {
   // fetch request for mls data
   // const getMlsdata = async () => {
   //   try {
-  //     const response = await fetch('https://users.mciserver.com/v1/js/loaderPublic.js',
+  //     const response = await fetch('https://users.mciserver.com/api/appSetup',
   //       {
   //         method: 'GET',
   //         headers: {
@@ -30,16 +30,16 @@ const SearchResults = () => {
     <div className="searchResultsContainer">
       <h1>Search Results</h1>
 
-      <div className="mci-app-node mci-idx-node mci-idx-node-quickSearch" data-mod="quickSearch" data-property_types="Residential,Condominium,Vacant Land" data-mls_box="true" data-district_list="true" data-price_range="true" data-bed_bath="true">
+      {/* <div className="mci-app-node mci-idx-node mci-idx-node-quickSearch" data-mod="quickSearch" data-property_types="Residential,Condominium,Vacant Land" data-mls_box="true" data-district_list="true" data-price_range="true" data-bed_bath="true">
         
         <div style={{textAlign: "center"}}>Loading...</div>
         
-      </div>
-
-
-      {/* <div className="mci-app-node mci-idx-node mci-idx-node-search" data-mod="search" data-display="featured">
-        <div style={{textAlign: "center"}}>Loading...</div>
       </div> */}
+
+
+      <div className="mci-app-node mci-idx-node mci-idx-node-search" data-mod="search" data-display="featured">
+        <div style={{textAlign: "center"}}>Loading...</div>
+      </div>
 
     </div>
   )
